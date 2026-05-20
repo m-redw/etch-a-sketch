e56d086a882c9aebae82aa6d5747b999e8e5e5ba
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-20 11:15:41 -0700

    Set up hover effect
    Grid divs change color when mouse passes over them.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,11 @@ function createSquare() {
     const square = document.createElement("div");
     square.style.width = "16px";
     square.style.height = "16px";
+
+    square.addEventListener("mouseenter", function() {
+        square.style.backgroundColor = "black";
+    })
+
     container.appendChild(square);
 }
 
