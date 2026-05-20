554d83c6c9cbfa12860ce6435e542c5fd0e352d6
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-20 11:37:09 -0700

    Add button to change number of squares per side
    Button will get user input, then delete existing grid, then add new
    grid based on user input. Max number of squares is 100.

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
+const container = document.querySelector(".container");
+
 function createSquare() {
-    const container = document.querySelector(".container");
     const square = document.createElement("div");
+    square.classList.add("square");
     square.style.width = "16px";
     square.style.height = "16px";
 
     square.addEventListener("mouseenter", function() {
         square.style.backgroundColor = "black";
-    })
+    });
 
     container.appendChild(square);
 }
 
-for (let i = 0; i < 256; i++) {
-    createSquare();
+function deleteCanvas() {
+    const canvasDivs = document.querySelectorAll(".square");
+    canvasDivs.forEach(function(square){
+        square.remove();
+    });
 }
+
+function createCanvas(width) {
+    container.style.width = `${16*width}px`;
+    container.style.height = `${16*width}px`;
+
+    for (let i = 0; i < width*width; i++) {
+        createSquare();
+    }
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", function(){
+    let strWidth = prompt("Enter number of squares per side. Ex: 100", "16");
+    let canvasWidth = Number(strWidth);
+    if (canvasWidth) {
+        deleteCanvas();
+        createCanvas(canvasWidth);
+    }
+});
+
+createCanvas(16);
